1abcd1038a78c9fedaed616569778b5339d2eaeb
Matthew Brisebois <matthew@webheroes.ca>
2023-04-24 14:29:26 -0600

    Fix node entry additions

--- src/node.js
+++ src/node.js
@@ -1,21 +1,34 @@
 
 import crypto				from 'crypto';
 
-global.crypto				= {
-    getRandomValues ( arr ) {
-	return crypto.randomBytes( arr.length );
-    },
-    "subtle": {
-	digest ( _, bytes ) {
-	    if ( bytes instanceof ArrayBuffer ) // Conversion needed for @noble/ed25519
-		bytes			= new Uint8Array( bytes );
-	    const hash			= crypto.createHash('sha512')
-	    hash.update( bytes );
-	    const digest		= hash.digest();
-	    return digest;
-	},
-    },
-};
+if ( global.crypto === undefined )
+    global.crypto			= {};
+
+if ( global.crypto.getRandomValues === undefined ) {
+    global.crypto.getRandomValues	= ( arr ) => crypto.randomBytes( arr.length );
+}
+
+if ( global.crypto.subtle?.digest === undefined ) {
+    if ( global.crypto.subtle === undefined )
+	global.crypto.subtle		= {};
+
+    global.crypto.subtle.digest		= ( _, bytes ) => {
+	if ( bytes instanceof ArrayBuffer ) // Conversion needed for @noble/ed25519
+	    bytes			= new Uint8Array( bytes );
+	const hash			= crypto.createHash('sha512');
+	hash.update( bytes );
+	const digest			= hash.digest();
+	return digest;
+    };
+}
+else {
+    const _digest			= global.crypto.subtle.digest;
+    global.crypto.subtle.digest		= ( algorithm, bytes ) => {
+	if ( bytes instanceof ArrayBuffer ) // Conversion needed for @noble/ed25519
+	    bytes			= new Uint8Array( bytes );
+	return _digest( algorithm, bytes );
+    }
+}
 
 import DefaultExports			from './index.js';
 
